2e62a18f1d5c9ca1b7165ef6ca9907c85b169726
Jonny Goff-White <jhg14@ic.ac.uk>
2017-03-24 13:00:46 +0000

    Add unit vectors to Orientation

--- src/Orientation.java
+++ src/Orientation.java
@@ -2,5 +2,13 @@
  * Created by JHGWhite on 24/03/2017.
  */
 public enum Orientation {
-    N, E, S, W
+    N(0, 1), E(1, 0), S(0, -1), W(-1, 0);
+
+    int i;
+    int j;
+
+    Orientation(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
 }
