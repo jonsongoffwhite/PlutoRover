2ed8435fbe24b9f6587c3c6c6052d390c96c5a13
Jonny Goff-White <jhg14@ic.ac.uk>
2017-03-24 12:53:18 +0000

    Add rover tests for backward and rotation in other direction

--- tests/RoverTest.java
+++ tests/RoverTest.java
@@ -21,7 +21,19 @@ public class RoverTest {
     }
 
     @Test
-    public void rotationCommandTest() throws Exception {
+    public void backwardCommandTest() throws Exception {
+
+        Rover rover = new Rover(1, 1, Orientation.N);
+
+        rover.run("B");
+
+        assertThat(rover.getX(), is(1));
+        assertThat(rover.getY(), is(0));
+        assertThat(rover.getOrientation(), is(Orientation.N));
+    }
+
+    @Test
+    public void rotationLeftCommandTest() throws Exception {
 
         Rover rover = new Rover(0, 0, Orientation.N);
 
@@ -30,7 +42,18 @@ public class RoverTest {
         assertThat(rover.getX(), is(0));
         assertThat(rover.getY(), is(0));
         assertThat(rover.getOrientation(), is(Orientation.W));
+    }
 
+    @Test
+    public void rotationRightCommandTest() throws Exception {
+
+        Rover rover = new Rover(0, 0, Orientation.N);
+
+        rover.run("R");
+
+        assertThat(rover.getX(), is(0));
+        assertThat(rover.getY(), is(0));
+        assertThat(rover.getOrientation(), is(Orientation.E));
     }
 
 }
