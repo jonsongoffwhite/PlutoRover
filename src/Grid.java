877238c913875dfe3ba22713ed8094869d780ddb
Jonny Goff-White <jhg14@ic.ac.uk>
2017-03-24 14:05:05 +0000

    Add obstacle map in Grid for obstacle detection

--- src/Grid.java
+++ src/Grid.java
@@ -1,14 +1,31 @@
+import java.util.HashMap;
+
 /**
  * Created by JHGWhite on 24/03/2017.
  */
 public class Grid {
 
+    // Width of the grid
     public final int xSize;
+
+    // Height of the grid
     public final int ySize;
+
+    // Map representing which co-ordinates contain obstacles
+    public final boolean[][] obstacleMap;
 
     public Grid(int xSize, int ySize) {
         this.xSize = xSize;
         this.ySize = ySize;
+
+        this.obstacleMap = new boolean[xSize][ySize];
+    }
+
+    public Grid(int xSize, int ySize, boolean[][] obstacleMap) {
+        this.xSize = xSize;
+        this.ySize = ySize;
+
+        this.obstacleMap = obstacleMap;
     }
 
 }
