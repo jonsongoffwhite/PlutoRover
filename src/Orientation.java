f9479d396c0f071ceeb4111ca9f1094dda30810c
Jonny Goff-White <jhg14@ic.ac.uk>
2017-03-24 12:39:27 +0000

    Add Rover boilerplate

---
+++ src/Orientation.java
@@ -0,0 +1,6 @@
+/**
+ * Created by JHGWhite on 24/03/2017.
+ */
+public enum Orientation {
+    N, E, S, W
+}
---
+++ src/Rover.java
@@ -0,0 +1,27 @@
+/**
+ * Created by JHGWhite on 24/03/2017.
+ */
+public class Rover {
+
+    // Rover's position properties
+    private int x;
+    private int y;
+    private Orientation orientation;
+
+    public Rover(int x, int y, Orientation orientation) {
+        this.x = x;
+        this.y = y;
+        this.orientation = orientation;
+    }
+
+    // Runs a string of commands
+    public void run(String commands) {
+
+    }
+
+    // Executes a single command
+    private void execute(String command) {
+
+    }
+
+}
