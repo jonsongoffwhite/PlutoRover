650da2eee17cb7dc27cd45674a4897760e2aa62c
Jonny Goff-White <jhg14@ic.ac.uk>
2017-03-24 13:40:29 +0000

    Add grid class for boundary detection, and in future, obstacle detection

---
+++ src/Grid.java
@@ -0,0 +1,14 @@
+/**
+ * Created by JHGWhite on 24/03/2017.
+ */
+public class Grid {
+
+    public final int xSize;
+    public final int ySize;
+
+    public Grid(int xSize, int ySize) {
+        this.xSize = xSize;
+        this.ySize = ySize;
+    }
+
+}
