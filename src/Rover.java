98a03f7c985f95031fc1d6b92be2397d4d010849
Jonny Goff-White <jhg14@ic.ac.uk>
2017-03-24 12:47:00 +0000

    Add getters for Rover properties

--- src/Rover.java
+++ src/Rover.java
@@ -14,6 +14,10 @@ public class Rover {
         this.orientation = orientation;
     }
 
+    public int getX() {return x;}
+    public int getY() {return y;}
+    public Orientation getOrientation() {return orientation;}
+
     // Runs a string of commands
     public void run(String commands) {
 
